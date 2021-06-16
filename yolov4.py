e59e1cc28992da7ab3f3f7a1095125a910171e5e
Asadullah-Dal17 <aiphile17@gmail.com>
2021-06-16 22:15:10 +0500

    udating yolov4 video code

--- yolov4.py
+++ yolov4.py
@@ -1,44 +1,38 @@
-import cv2
-import time
+import cv2 as cv
+Conf_threshold = 0.4
+NMS_threshold = 0.4
+COLORS = [(0, 255, 0), (0, 0, 255), (255, 0, 0),
+          (255, 255, 0), (255, 0, 255), (0, 255, 255)]
 
-CONFIDENCE_THRESHOLD = 0.2
-NMS_THRESHOLD = 0.4
-COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+class_name = []
+with open('classes.txt', 'r') as f:
+    class_name = [cname.strip() for cname in f.readlines()]
+# print(class_name)
+net = cv.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
 
-class_names = []
-with open("classes.txt", "r") as f:
-    class_names = [cname.strip() for cname in f.readlines()]
-
-vc = cv2.VideoCapture(0)
-
-net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg")
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-
-model = cv2.dnn_DetectionModel(net)
+model = cv.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-while cv2.waitKey(1) < 1:
-    (grabbed, frame) = vc.read()
-    if not grabbed:
-        exit()
 
-    start = time.time()
-    classes, scores, boxes = model.detect(
-        frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-    end = time.time()
+cap = cv.VideoCapture('output.avi')
 
-    start_drawing = time.time()
+while True:
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    classes, scores, boxes = model.detect(frame, Conf_threshold, NMS_threshold)
     for (classid, score, box) in zip(classes, scores, boxes):
         color = COLORS[int(classid) % len(COLORS)]
-        label = "%s : %f" % (class_names[classid[0]], score)
-        cv2.rectangle(frame, box, color, 2)
-        cv2.putText(frame, label, (box[0], box[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    end_drawing = time.time()
+        label = "%s : %f" % (class_name[classid[0]], score)
+        cv.rectangle(frame, box, color, 1)
+        cv.putText(frame, label, (box[0], box[1]-10),
+                   cv.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
 
-    fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (
-        1 / (end - start), (end_drawing - start_drawing) * 1000)
-    cv2.putText(frame, fps_label, (0, 25),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-    cv2.imshow("detections", frame)
+    cv.imshow('frame', frame)
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
